d812e9bd4302c8f0c623ee47320e5880cbe7448a
Sarar Hammar <hammar.sarar@gmail.com>
2025-08-05 00:10:48 +0200

    Commit initial

---
+++ src/main/java/com/openclassrooms/PayMyBuddyAPIWeb/PayMyBuddyApiWebApplication.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.PayMyBuddyAPIWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayMyBuddyApiWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayMyBuddyApiWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/openclassrooms/PayMyBuddyAPIWeb/PayMyBuddyApiWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.PayMyBuddyAPIWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayMyBuddyApiWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
