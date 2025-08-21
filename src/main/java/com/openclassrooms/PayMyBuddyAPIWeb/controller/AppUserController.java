4478c907db2e9f3fb45fb7d6ad77f180b0444d9f
Sarar Hammar <hammar.sarar@gmail.com>
2025-08-21 04:15:03 +0200

    feat: ajout du contrôleur REST AppUserController
    - Création d’un RestController pour l’URL /api/users.
    - Endpoints GET : récupérer tous les utilisateurs, par ID, par username, par email.
    - Endpoint POST : création d’un utilisateur avec validation des données (@Valid sur AppUserDTO).

---
+++ src/main/java/com/openclassrooms/PayMyBuddyAPIWeb/controller/AppUserController.java
@@ -0,0 +1,49 @@
+package com.openclassrooms.PayMyBuddyAPIWeb.controller;
+
+import com.openclassrooms.PayMyBuddyAPIWeb.dto.AppUserDTO;
+import com.openclassrooms.PayMyBuddyAPIWeb.service.AppUserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class AppUserController {
+
+    @Autowired
+    private AppUserService appUserService;
+
+    @GetMapping
+    public List<AppUserDTO> getAllUsers() {
+        return appUserService.getAllUsers();
+    }
+
+    @GetMapping("id/{id}")
+    public ResponseEntity<AppUserDTO> getUserById(@PathVariable int id) {
+        return appUserService.getUserById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("userName/{userName}")
+    public ResponseEntity<AppUserDTO> getUserByUserName(@PathVariable String userName){
+        return appUserService.getUserByUserName(userName)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("email/{email}")
+    public ResponseEntity<AppUserDTO> getUserByEmail(@PathVariable String email) {
+        return appUserService.getUserByEmail(email)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<AppUserDTO> createUser(@Valid @RequestBody AppUserDTO appUserDTO){
+        return ResponseEntity.ok(appUserService.createUser(appUserDTO));
+    }
+}
