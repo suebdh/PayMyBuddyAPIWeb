ad2ab664a69d5da5153b35cfbdfda821b276d790
Sarar Hammar <hammar.sarar@gmail.com>
2025-08-23 16:08:27 +0200

    feat(service): améliorer la gestion des utilisateurs et la sécurité des mots de passe
    - Vérification de l’unicité des emails lors de la création et mise à jour (EmailAlreadyUsedException si non unique)
    - Hashage des mots de passe avec PasswordEncoder avant sauvegarde
    - Ajout de la méthode updateUser pour gérer la mise à jour des utilisateurs (UserNotFoundException si non trouvé)
    - Refactorisation interne pour clarifier le flux de création et mise à jour des utilisateurs (validation, hashage, sauvegarde)

--- src/main/java/com/openclassrooms/PayMyBuddyAPIWeb/controller/LoginController.java
+++ src/main/java/com/openclassrooms/PayMyBuddyAPIWeb/controller/LoginController.java
@@ -2,13 +2,9 @@ package com.openclassrooms.PayMyBuddyAPIWeb.controller;
 
 import com.openclassrooms.PayMyBuddyAPIWeb.dto.LoginDTO;
 import com.openclassrooms.PayMyBuddyAPIWeb.service.LoginService;
-import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
--- src/main/java/com/openclassrooms/PayMyBuddyAPIWeb/service/AppUserService.java
+++ src/main/java/com/openclassrooms/PayMyBuddyAPIWeb/service/AppUserService.java
@@ -2,8 +2,11 @@ package com.openclassrooms.PayMyBuddyAPIWeb.service;
 
 import com.openclassrooms.PayMyBuddyAPIWeb.dto.AppUserDTO;
 import com.openclassrooms.PayMyBuddyAPIWeb.entity.AppUser;
+import com.openclassrooms.PayMyBuddyAPIWeb.exception.EmailAlreadyUsedException;
+import com.openclassrooms.PayMyBuddyAPIWeb.exception.UserNotFoundException;
 import com.openclassrooms.PayMyBuddyAPIWeb.repository.AppUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,6 +18,16 @@ public class AppUserService {
 
     @Autowired
     private AppUserRepository appUserRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    // Vérifier si l'email existe déjà
+    private void validateEmailUnique(String email) {
+        if (appUserRepository.findByEmail(email).isPresent()) {
+            throw new EmailAlreadyUsedException("Email déjà utilisé !");
+        }
+    }
 
     public List<AppUserDTO> getAllUsers(){
         return ((List<AppUser>) appUserRepository.findAll())
@@ -39,7 +52,32 @@ public class AppUserService {
     }
 
     public AppUserDTO createUser(AppUserDTO appUserDTO){
+        validateEmailUnique(appUserDTO.getEmail());
+        // Sauvegarder le nouvel utilisateur après hashage du mpt de passe
+        appUserDTO.setPassword(passwordEncoder.encode(appUserDTO.getPassword()));
         return convertToDTO(appUserRepository.save(convertToEntity(appUserDTO)));
+    }
+
+    public AppUserDTO updateUser(int userId, AppUserDTO appUserDTO) {
+        // 1- Récupérer l'utilisateur existant
+        AppUser existingUser = appUserRepository.findById(userId)
+                .orElseThrow(() -> new UserNotFoundException("Utilisateur introuvable"));
+
+        // 2- Vérifier si l'email souhaité est déjà pris (On passe l'email du DTO, donc ce que l'utilisateur VEUT mettre)
+        // On vérifie l'email seulement si l'utilisateur veut le changer
+        if (!existingUser.getEmail().equals(appUserDTO.getEmail())) {
+            validateEmailUnique(appUserDTO.getEmail());
+        }
+
+        // 3- Mettre à jour les champs
+        existingUser.setUserName(appUserDTO.getUserName());
+        existingUser.setEmail(appUserDTO.getEmail());
+        existingUser.setPassword(passwordEncoder.encode(appUserDTO.getPassword()));
+        existingUser.setBalance(appUserDTO.getBalance());
+        // TODO A verifier plus tard si j'aurais d'autres champs ajoutés et donc à setter également
+
+        // 4- Sauvegarder et retourner le DTO
+        return convertToDTO(appUserRepository.save(existingUser));
     }
 
     public AppUserDTO convertToDTO(AppUser appUser){
